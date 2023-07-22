0b2688ba92c64ea871e738228815f7ef2ea95b7b
handika <handikaalfalaq01@gmail.com>
2023-07-22 19:25:15 +0700

    istall package

--- client/src/config/Api.js
+++ client/src/config/Api.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
 export const API = axios.create({
-  baseURL: 'http://localhost:5000/ktmstore/api/v1',
+  baseURL: process.env.REACT_APP_BASE_URL
 });
  
-
-// Set Authorization Token Header
 export const setAuthToken = (token) => {
   if (token) {
     API.defaults.headers.common['Authorization'] = `Bearer ${token}`;
